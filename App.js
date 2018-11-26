0afc65307d3b6a93cc3cfc67758c5def4bffb9f3
dhananjay27 <dmw7044@gmail.com>
2018-11-26 14:42:48 +0530

    hello here

--- App.js
+++ App.js
@@ -16,16 +16,15 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
 
-        <Text style={styles.welcome}>Welcome to Demo Appication Vivek Vithalani</Text>
+        <Text style={styles.welcome}>Welcome to Demo Appication Shivani mahidaa</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
 
-        <Text style={styles.welcome}>Welcome to Demo Appication Changes from Vivek Vithalani account</Text>
+        <Text style={styles.welcome}>Welcome to Demo Appication Changes from Shivani  account</Text>
         <Text style={styles.instructions}>Testing Dhananjay changes 123456 with email id and user name</Text>
 
         <Text style={styles.instructions}>{instructions}</Text>
